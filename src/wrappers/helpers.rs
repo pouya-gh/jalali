64e5280a9ba1e42a11138c9359e526e69eb30a9c
Pouya Gharib Pour <p.gharibpour@gmail.com>
2016-04-30 11:03:09 +0430

    define helper funtions wrappers

---
+++ src/wrappers/helpers.rs
@@ -0,0 +1,32 @@
+use libc::time_t;
+use std::ffi::CString;
+use std::mem;
+use jalali_bindings::*;
+
+pub fn is_year_leap(year: i32) -> bool {
+  let tmp = unsafe {
+    jalali_is_jleap(year)
+  };
+  if tmp == 0 {
+    false
+  } else {
+    true
+  }
+}
+
+pub fn number_of_month_days(year: i32, month: i32) -> i32 {
+  unsafe {
+    jalali_year_month_days(year, month)
+  }
+}
+
+pub fn time_to_string(time: i64) -> String {
+  let mut buf = Vec::<u8>::with_capacity(27);
+  let mut result: String;
+  unsafe {
+    jctime_r(&time, buf.as_mut_ptr() as *mut i8);
+    result = CString::from_raw(buf.as_mut_ptr() as *mut i8).into_string().unwrap();
+  }
+  mem::forget(buf);
+  return result;
+}
--- src/wrappers/mod.rs
+++ src/wrappers/mod.rs
@@ -1,3 +1,4 @@
 mod jtm;
 mod ab_jtm;
 mod jy_info;
+mod helpers;
