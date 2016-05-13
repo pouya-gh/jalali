4df67b1d07d4587ee9b8be88ceca14e3993467fa
Pouya Gharib Pour <p.gharibpour@gmail.com>
2016-05-13 18:01:31 +0430

    write documentations for wrappers::jtm module

--- src/wrappers/jtm.rs
+++ src/wrappers/jtm.rs
@@ -1,9 +1,33 @@
+//! This module holds the definitions of JTMBuilder and
+//! JTM related bindings.
+
 use libc::{c_int, time_t, size_t};
 use data_structs::{JTM};
 use jalali_bindings::*;
 use std::ffi::CString;
 use std::{mem, ptr};
 
+/// This struct serves as a builder for JTM struct.
+///
+/// # Examples
+///
+/// ```
+/// extern crate jalali;
+///
+/// jalali::JTMBuilder::new().set_sec(11).
+///                           set_min(22).
+///                           set_hour(33).
+///                           set_mday(44).
+///                           set_mon(55).
+///                           set_year(66).
+///                           set_wday(77).
+///                           set_yday(88).
+///                           set_isdst(1).
+///                           set_gmtoff(99).
+///                           set_zone("IRST".to_string()).
+///                           build();
+///
+/// ```
 pub struct JTMBuilder {
   pub tm_sec: i32,
   pub tm_min: i32,
@@ -19,6 +43,8 @@ pub struct JTMBuilder {
 }
 
 impl JTMBuilder {
+  /// This function initializes a JTMBuilder struct.
+  ///
   pub fn new() -> Self {
     JTMBuilder {
       tm_sec: 0,
@@ -35,61 +61,129 @@ impl JTMBuilder {
     }
   }
 
+  /// This method sets tm_sec of a JTMBuilder struct.
+  ///
+  /// # Arguments
+  ///
+  /// * `sec` - A 32 bit integer providing the value for *tm_sec*.
+  ///
   pub fn set_sec(&mut self, sec: i32) -> &mut Self {
     self.tm_sec = sec;
     self
   }
 
+  /// This method sets tm_min of a JTMBuilder struct.
+  ///
+  /// # Arguments
+  ///
+  /// * `min` - A 32 bit integer providing the value for *tm_min*.
+  ///
   pub fn set_min(&mut self, min: i32) -> &mut Self {
     self.tm_min = min;
     self
   }
 
+  /// This method sets tm_hour of a JTMBuilder struct.
+  ///
+  /// # Arguments
+  ///
+  /// * `hour` - A 32 bit integer providing the value for *tm_hour*.
+  ///
   pub fn set_hour(&mut self, hour: i32) -> &mut Self {
     self.tm_hour = hour;
     self
   }
 
+  /// This method sets tm_mday of a JTMBuilder struct.
+  ///
+  /// # Arguments
+  ///
+  /// * `mday` - A 32 bit integer providing the value for *tm_mday*.
+  ///
   pub fn set_mday(&mut self, mday: i32) -> &mut Self {
     self.tm_mday = mday;
     self
   }
 
+  /// This method sets tm_mon of a JTMBuilder struct.
+  ///
+  /// # Arguments
+  ///
+  /// * `mon` - A 32 bit integer providing the value for *tm_mon*.
+  ///
   pub fn set_mon(&mut self, mon: i32) -> &mut Self {
     self.tm_mon = mon;
     self
   }
 
+  /// This method sets tm_year of a JTMBuilder struct.
+  ///
+  /// # Arguments
+  ///
+  /// * `year` - A 32 bit integer providing the value for *tm_year*.
+  ///
   pub fn set_year(&mut self, year: i32) -> &mut Self {
     self.tm_year = year;
     self
   }
 
+  /// This method sets tm_wday of a JTMBuilder struct.
+  ///
+  /// # Arguments
+  ///
+  /// * `wday` - A 32 bit integer providing the value for *tm_wday*.
+  ///
   pub fn set_wday(&mut self, wday: i32) -> &mut Self {
     self.tm_wday = wday;
     self
   }
 
+  /// This method sets tm_yday of a JTMBuilder struct.
+  ///
+  /// # Arguments
+  ///
+  /// * `yday` - A 32 bit integer providing the value for *tm_yday*.
+  ///
   pub fn set_yday(&mut self, yday: i32) -> &mut Self {
     self.tm_yday = yday;
     self
   }
 
+  /// This method sets tm_isdst of a JTMBuilder struct.
+  ///
+  /// # Arguments
+  ///
+  /// * `isdst` - A 32 bit integer providing the value for *tm_isdst*.
+  ///
   pub fn set_isdst(&mut self, isdst: i32) -> &mut Self {
     self.tm_isdst = isdst;
     self
   }
 
+  /// This method sets tm_gmtoff of a JTMBuilder struct.
+  ///
+  /// # Arguments
+  ///
+  /// * `gmtoff` - A 64 bit integer providing the value for *tm_gmtoff*.
+  ///
   pub fn set_gmtoff(&mut self, gmtoff: i64) -> &mut Self {
     self.tm_gmtoff = gmtoff;
     self
   }
 
+  /// This method sets tm_zone of a JTMBuilder struct.
+  ///
+  /// # Arguments
+  ///
+  /// * `zone` - A String providing the value for *tm_zone*.
+  ///
   pub fn set_zone(&mut self, zone: String) -> &mut Self {
     self.tm_zone = zone;
     self
   }
 
+  /// This method builds a JTM and returns it.
+  ///
   pub fn build(&self) -> JTM {
     JTM {
       tm_sec: self.tm_sec,
@@ -110,6 +204,20 @@ impl JTMBuilder {
 }
 
 impl JTM {
+  /// This function initializes a JTM from seconds passed since Epoch.
+  ///
+  /// # Arguments
+  ///
+  /// * `secs` - A 64 bit integer representing seconds passed since Epoch.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// extern crate jalali;
+  ///
+  /// jalali::JTM::from_secs(719425800);
+  ///
+  /// ```
   pub fn from_secs(secs: i64) -> Self {
     let mut result: Self;
     unsafe {
@@ -119,6 +227,20 @@ impl JTM {
     result
   }
 
+  /// This function initializes a JTM from days passed since Epoch.
+  ///
+  /// # Arguments
+  ///
+  /// * `days` - A 32 bit integer representing days passed since Epoch.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// extern crate jalali;
+  ///
+  /// jalali::JTM::from_days(8326);
+  ///
+  /// ```
   pub fn from_days(days: i32) -> Self {
     let mut result: Self;
     unsafe {
@@ -128,6 +250,20 @@ impl JTM {
     result
   }
 
+  /// This function initializes a JTM from seconds passed since Epoch expressed in Coordinated Universal Time (UTC).
+  ///
+  /// # Arguments
+  ///
+  /// * `secs` - A 64 bit integer representing seconds passed since Epoch.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// extern crate jalali;
+  ///
+  /// jalali::JTM::from_days(8326);
+  ///
+  /// ```
   pub fn from_secs_to_utc(secs: i64) -> Self {
     let mut result: Self;
     unsafe {
@@ -137,6 +273,23 @@ impl JTM {
     result
   }
 
+  /// This function initializes a JTM from a string with a special format.
+  ///
+  /// # Arguments
+  ///
+  /// * `s` - A string representing time.
+  /// * `format` - A string representing format of *s* (see http://www.nongnu.org/jcal/jstrptime.html).
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// extern crate jalali;
+  ///
+  /// let tmp1 = "26/07/1371 19:59:59".to_string();
+  /// let tmp2 = "%d/%m/%Y %H:%M:%S".to_string();
+  /// jalali::JTM::from_formatted_string(&tmp1, &tmp2).unwrap();
+  ///
+  /// ```
   pub fn from_formatted_string(s: &String, format: &String) -> Result<Self,()> {
     let mut tmp_s = s.clone();
     let mut tmp_f = format.clone();
@@ -158,12 +311,37 @@ impl JTM {
     }
   }
 
+  /// This method initializes a JTM from a string with a special format.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// extern crate jalali;
+  ///
+  /// let tmp = jalali::JTM::from_days(8326);
+  /// assert_eq!(tmp.get_timezone(), "IRST".to_string());
+  ///
+  /// ```
   pub fn get_timezone(&self) -> String {
     let mut tst = String::with_capacity(5);
     self.make_formated_string(&"%Z".to_string(), &mut tst);
     tst
   }
 
+  /// This method alters tm_mon and tm_mday fields of the broken-down
+  /// jalali time strucutre based on it's tm_yday field.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// extern crate jalali;
+  ///
+  /// let mut j = jalali::JTMBuilder::new().set_yday(211).build();
+  /// j.create_date_from_days().unwrap();
+  /// assert_eq!(j.tm_mon, 6);
+  /// assert_eq!(j.tm_mday, 26);
+  ///
+  /// ```
   pub fn create_date_from_days(&mut self) -> Result<(),()> {
     let tmp = unsafe {
       jalali_create_date_from_days(self)
@@ -174,6 +352,19 @@ impl JTM {
     }
   }
 
+  /// This method alters tm_yday field of the broken-down jalali
+  /// time structure based on it's tm_mon and tm_mday fields.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// extern crate jalali;
+  ///
+  /// let mut j = jalali::JTMBuilder::new().set_mday(26).set_mon(6).build();
+  /// j.create_days_from_date().unwrap();
+  /// assert_eq!(j.tm_yday, 211);
+  ///
+  /// ```
   pub fn create_days_from_date(&mut self) -> Result<(),()> {
     let tmp = unsafe {
       jalali_create_days_from_date(self)
@@ -184,24 +375,77 @@ impl JTM {
     }
   }
 
+  /// This method calculates the number of days passed since UTC
+  /// Epoch based on a broken-down jalali time structure supplied to it.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// extern crate jalali;
+  ///
+  /// let tmp = jalali::JTM::from_secs_to_utc(719425800);
+  /// assert_eq!(tmp.get_days_since_epoch(), 8326);
+  ///
+  /// ```
   pub fn get_days_since_epoch(&self) -> i32 {
     unsafe {
       jalali_get_diff(self)
     }
   }
 
+  /// This method updates tm_wday and tm_yday fields of the broken-down
+  /// jalali time structure based on it's tm_year, tm_mon, tm_mday,
+  /// tm_hour, tm_min and tm_sec fields. If structure members are outside
+  /// their valid interval, they will be normalized (so that, for example,
+  /// 40 Bahman is changed into 10 Esfand). tm_isdst, tm_gmtoff and tm_zone
+  /// fields are set accordingly.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// extern crate jalali;
+  ///
+  /// let mut j = jalali::JTMBuilder::new().set_sec(59).
+  ///                                        set_min(59).
+  ///                                        set_hour(19).
+  ///                                        set_mday(26).
+  ///                                        set_mon(6).
+  ///                                        set_year(1371).
+  ///                                        set_isdst(0).
+  ///                                        set_gmtoff(12600).
+  ///                                        set_zone("IRST".to_string()).
+  ///                                        build();
+  /// j.update();
+  /// assert_eq!(j.tm_wday, 1);
+  /// assert_eq!(j.tm_yday, 211);
+  ///
+  /// ```
   pub fn update(&mut self) {
     unsafe {
       jalali_update(self);
     }
   }
 
+  /// This method writes a string representation of the struct
+  /// to standard output.
+  ///
   pub fn show_time(&self) {
     unsafe {
       jalali_show_time(self);
     }
   }
 
+  /// This method returns a string representation of the struct.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// extern crate jalali;
+  ///
+  /// let mut j = jalali::JTM::from_secs_to_utc(719425800);
+  /// assert_eq!(j.to_string(), "Yek Meh 26 16:30:00 1371\n".to_string());
+  ///
+  /// ```
   pub fn to_string(&self) -> String {
     let mut buff = Vec::<u8>::with_capacity(100);
     let result: String;
@@ -213,12 +457,47 @@ impl JTM {
     return result;
   }
 
+  /// This method converts a broken-down jalali time structure,
+  /// expressed as local time, to calendar time representation.
+  /// It ignores the values supplied by the caller in the tm_wday field.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// extern crate jalali;
+  ///
+  /// let mut j = jalali::JTM::from_secs(719425800);
+  /// assert_eq!(j.to_secs(), 719425800);
+  ///
+  /// ```
   pub fn to_secs(&mut self) -> i64 {
     unsafe {
       jmktime(self)
     }
   }
 
+  /// This method returns a string representation of the struct
+  /// based on a supplied *format*.
+  /// It returns the number of characters placed in *buff*.
+  /// If *buff* is not long enough, it returns max.
+  ///
+  /// # Arguments
+  ///
+  /// * `format` - A string representing format of *buff* (see http://www.nongnu.org/jcal/jstrftime.html).
+  /// * `buff` - A string serving as a buffer. It must be long enough to hold the resulting string.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// extern crate jalali;
+  ///
+  /// let f = "%H:%M".to_string();
+  /// let mut buffer = String::with_capacity(5);
+  /// let mut j = jalali::JTM::from_secs(719425800);
+  /// j.make_formated_string(&f, &mut buffer);
+  /// assert_eq!(buffer, "20:0".to_string());
+  ///
+  /// ```
   pub fn make_formated_string(&self, format: &String, buff: &mut String) -> usize {
     let mut b = String::with_capacity(buff.capacity()).into_bytes();
     let retval: usize;
