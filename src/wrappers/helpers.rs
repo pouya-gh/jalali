93cb8f72b0941cf081b045bb63817fe1cee07f51
Pouya Gharib Pour <p.gharibpour@gmail.com>
2016-05-13 22:01:20 +0430

    write documentations for wrappers::helpers module

--- src/wrappers/helpers.rs
+++ src/wrappers/helpers.rs
@@ -1,7 +1,26 @@
+//! This module holds the definitions of
+//! wrappers which do not belong to any
+//! data structures.
+
 use std::ffi::CString;
 use std::mem;
 use jalali_bindings::*;
 
+/// This function calculates whether a given year is leap.
+///
+/// # Arguments
+///
+/// * `year` - A 32 bit integer representing the year.
+///
+/// # Examples
+///
+/// ```
+/// extern crate jalali;
+///
+/// assert_eq!(jalali::is_year_leap(1370), true);
+/// assert_eq!(jalali::is_year_leap(1371), false);
+///
+/// ```
 pub fn is_year_leap(year: i32) -> bool {
   let tmp = unsafe {
     jalali_is_jleap(year)
@@ -13,12 +32,45 @@ pub fn is_year_leap(year: i32) -> bool {
   }
 }
 
+/// This function returns number of days of a specified month in a specified year.
+///
+/// # Arguments
+///
+/// * `year` - A 32 bit integer representing the year.
+/// * `month` - A 32 bit integer representing the month (0_11).
+///
+/// # Examples
+///
+/// ```
+/// extern crate jalali;
+///
+/// assert_eq!(jalali::number_of_month_days(1371, 0), 31);
+/// assert_eq!(jalali::number_of_month_days(1371, 6), 30);
+/// assert_eq!(jalali::number_of_month_days(1371, 11), 29);
+/// assert_eq!(jalali::number_of_month_days(1370, 11), 30); // leap
+///
+/// ```
 pub fn number_of_month_days(year: i32, month: i32) -> i32 {
   unsafe {
     jalali_year_month_days(year, month)
   }
 }
 
+/// This function converts time (specified in seconds elapsed since UTC Epoch)
+/// to string.
+///
+/// # Arguments
+///
+/// * `time` - A 64 bit integer representing number of seconds passed since UTC Epoch.
+///
+/// # Examples
+///
+/// ```
+/// extern crate jalali;
+///
+/// assert_eq!(jalali::time_to_string(719425800), "Yek Meh 26 20:00:00 1371\n".to_string());
+///
+/// ```
 pub fn time_to_string(time: i64) -> String {
   let mut buf = Vec::<u8>::with_capacity(27);
   let result: String;
