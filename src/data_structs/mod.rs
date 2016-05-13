1f80c42da5b5b970740441c89acb57310be1b563
Pouya Gharib Pour <p.gharibpour@gmail.com>
2016-05-13 18:00:49 +0430

    write documentations for data_structs module

--- src/data_structs/mod.rs
+++ src/data_structs/mod.rs
@@ -1,21 +1,37 @@
+//! This module holds definitions of data structures used in this library.
+
 use libc::{c_int, c_char, c_long};
 
+/// Broken-down jalali time
+///
 #[derive(Debug)]
 #[repr(C)]
 pub struct JTM {
-  pub tm_sec: c_int,       /* Seconds. (0-59) */
-  pub tm_min: c_int,       /* Minutes. (0-59) */
-  pub tm_hour: c_int,      /* Hours. (0-59) */
-  pub tm_mday: c_int,      /* Day of the month. (1-31) */
-  pub tm_mon: c_int,       /* Month. (0-11) */
-  pub tm_year: c_int,      /* Year. */
-  pub tm_wday: c_int,      /* Day of the week. (0-6) */
-  pub tm_yday: c_int,      /* Day in the year. (0-365) */
-  pub tm_isdst: c_int,     /* Daylight saving time is in effect. */
-  pub tm_gmtoff: c_long,   /* Seconds east of UTC. */
-  pub tm_zone: *const c_char,  /* Timezone abbreviation.  */
+  /// Seconds. (0-59)
+  pub tm_sec: c_int,
+  /// Minutes. (0-59)
+  pub tm_min: c_int,
+  /// Hours. (0-59)
+  pub tm_hour: c_int,
+  /// Day of the month. (1-31)
+  pub tm_mday: c_int,
+  /// Month. (0-11)
+  pub tm_mon: c_int,
+  /// Year.
+  pub tm_year: c_int,
+  /// Day of the week. (0-6)
+  pub tm_wday: c_int,
+  /// Day in the year. (0-365)
+  pub tm_yday: c_int,
+  /// Daylight saving time is in effect.
+  pub tm_isdst: c_int,
+  /// Seconds east of UTC.
+  pub tm_gmtoff: c_long,
+  /// Timezone abbreviation.
+  pub tm_zone: *const c_char,
 }
 
+/// Information about passed days since Epoch
 #[derive(Debug)]
 #[repr(C)]
 pub struct AB_JTM {
@@ -25,14 +41,23 @@ pub struct AB_JTM {
   pub ab_days: c_int,
 }
 
+/// Information about a certain year in jalali system
+///
 #[derive(Debug)]
 #[repr(C)]
 pub struct JYInfo {
-  pub lf: c_int,          /* leap indicator flag */
-  pub y: c_int,           /* year */
-  pub r: c_int,           /* reamining years in grand cycle */
-  pub p: c_int,           /* passed years from grand cycle*/
-  pub rl: c_int,          /* remaining leap years in grand cycle */
-  pub pl: c_int,          /* passed leap years in grand cycle */
-  pub apl: c_int,         /* absolute passed leaps */
+  /// leap indicator flag
+  pub lf: c_int,
+  /// year
+  pub y: c_int,
+  /// reamining years in grand cycle
+  pub r: c_int,
+  /// passed years from grand cycle
+  pub p: c_int,
+  /// remaining leap years in grand cycle
+  pub rl: c_int,
+  /// passed leap years in grand cycle
+  pub pl: c_int,
+  /// absolute passed leaps
+  pub apl: c_int,
 }
